3d85783d8891e3796a53326ec66079c2a075588c
Robin Lin <lbl52001@gmail.com>
2025-02-08 15:36:56 +0100

    Add chagpt-el

--- init.el
+++ init.el
@@ -1,34 +1,36 @@
-(setq gc-cons-threshold (* 128 1024 1024))
+;; -*- lexical-binding: t; -*-
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(defun config/add-subdirs-to-load-path (parent-dir)
+  (let* ((default-directory parent-dir))
+    (setq load-path (append (seq-filter (lambda (dir) (file-directory-p dir))
+                                        (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
+                            load-path))
+    )
+  )
+
+(setq gc-cons-threshold (* 128 1024 1024))
 
 (setq p-org-roam-directory "~/RoamNotes")
 (let ((private-file (expand-file-name ".private.el" user-emacs-directory)))
   (when (file-exists-p private-file)
     (load-file private-file))
   )
+(config/add-subdirs-to-load-path
+ (expand-file-name "site-lisp/" user-emacs-directory))
 
 (setq package-enable-at-startup nil)
 (require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu"          . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("nongnu"       . "https://elpa.nongnu.org/nongnu/"))
+(unless package-archive-contents (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
-
-(defvar use-package-always-ensure)
 (setq use-package-always-ensure t)
-(defvar use-package-always-pin)
-(setq use-package-always-pin "melpa")
 
-
-;; GUI
+;; Font
 (if (or (eq system-type 'windows-nt) (eq system-type 'gnu/linux))
     (cond
      ((find-font (font-spec :name "Source Code Pro"))
@@ -85,7 +87,6 @@
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (setq inhibit-startup-screen t)
-(global-hl-line-mode +1)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -96,15 +97,11 @@
 (setq-default line-spacing 1)
 (show-paren-mode 1)
 
+;; Basic UI Cleanup
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode +1)
-
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-
-(windmove-default-keybindings)
-
+(global-hl-line-mode +1)
 (set-fringe-mode '(10 . 0))
 (when (boundp 'fringe-indicator-alist)
   (setq-default fringe-indicator-alist
@@ -119,11 +116,17 @@
 		  (empty-line . nil)
 		  (unknown . nil))))
 
+;; Key bindings
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(windmove-default-keybindings)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Themes
 (use-package modus-themes :demand t
   :init
   (load-theme 'modus-vivendi t)
   )
-(use-package solarized-theme :demand t)
 
 (use-package doom-modeline
   :ensure t
@@ -138,11 +141,13 @@
   ;;(doom-modeline-major-mode-color-icon t)
   (line-number-mode 1)
   (column-number-mode 1))
+
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode)
   )
 
 (use-package treemacs)
+
 (use-package winner
   :config
   (winner-mode t))
@@ -152,13 +157,10 @@
   (push '("*Warning*") popwin:special-display-config)
   )
 
+;; Completion Framework
 (use-package vertico
   :init
   (vertico-mode))
-
-(use-package ctrlf
-  :config
-  (ctrlf-mode +1))
 
 (use-package prescient
   :config
@@ -169,176 +171,25 @@
   :custom (completion-styles '(orderless)))
 
 (use-package marginalia
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
   :bind (("M-A" . marginalia-cycle)
          :map minibuffer-local-map
          ("M-A" . marginalia-cycle))
-
-  ;; The :init configuration is always executed (Not lazy!)
   :init
-
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode))
 
 (use-package consult
-  ;; Replace bindings. Lazily loaded due by `use-package'.
-  :bind (;; C-c bindings (mode-specific-map)
-         ("C-c h" . consult-history)
-         ("C-c m" . consult-mode-command)
-         ("C-c k" . consult-kmacro)
-         ;; C-x bindings (ctl-x-map)
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ;; ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ("<help> a" . consult-apropos)            ;; orig. apropos-command
-         ;; M-g bindings (goto-map)
-         ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings (search-map)
-         ("M-s d" . consult-find)
-         ("M-s D" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s m" . consult-multi-occur)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi))           ;; needed by consult-line to detect isearch
-
-  ;; Enable automatic preview at point in the *Completions* buffer. This is
-  ;; relevant when you use the default completion UI. You may want to also
-  ;; enable `consult-preview-at-point-mode` in Embark Collect buffers.
+  :bind (
+         ("C-x b" . consult-buffer)
+         ("C-s" . consult-line))
   :hook (completion-list-mode . consult-preview-at-point-mode)
-
-  ;; The :init configuration is always executed (Not lazy)
   :init
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
   (setq register-preview-delay 0
         register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  ;; Optionally replace `completing-read-multiple' with an enhanced version.
-  (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
-
-  ;; Use Consult to select xref locations with preview
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
-
-  ;; Configure other variables and modes in the :config section,
-  ;; after lazily loading the package.
-  :config
-
-  ;; Optionally configure preview. The default value
-  ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key (kbd "M-."))
-  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
-  ;; For some commands and buffer sources it is useful to configure the
-  ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  (consult-customize
-   consult-theme
-   :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-recent-file consult--source-project-recent-file consult--source-bookmark
-   :preview-key (kbd "M-."))
-
-  ;; Optionally configure the narrowing key.
-  ;; Both < and C-+ work reasonably well.
-  (setq consult-narrow-key "<") ;; (kbd "C-+")
-
-  ;; Optionally make narrowing help available in the minibuffer.
-  ;; You may want to use `embark-prefix-help-command' or which-key instead.
-  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-  ;; Optionally configure a function which returns the project root directory.
-  ;; There are multiple reasonable alternatives to chose from.
-  ;;;; 1. project.el (project-roots)
-  ;; (setq consult-project-root-function
-  ;;       (lambda ()
-  ;;         (when-let (project (project-current))
-  ;;           (car (project-roots project)))))
-  ;;;; 2. projectile.el (projectile-project-root)
-  ;; (autoload 'projectile-project-root "projectile")
-  ;; (setq consult-project-root-function #'projectile-project-root)
-  ;;;; 3. vc.el (vc-root-dir)
-  ;; (setq consult-project-root-function #'vc-root-dir)
-  ;;;; 4. locate-dominating-file
-  ;; (setq consult-project-root-function (lambda () (locate-dominating-file "." ".git")))
   )
 
-(use-package consult-dir
-  :ensure t
-  :bind (("C-x C-d" . consult-dir)
-         :map minibuffer-local-completion-map
-         ("C-x C-d" . consult-dir)
-         ("C-x C-j" . consult-dir-jump-file))
-  :config
-  (setq consult-dir-project-list-function nil)
-  (setq consult-dir-project-list-function #'consult-dir-projectile-dirs)
-  )
-
-(use-package embark
-  :ensure t
-
-  :bind
-  (("C-." . embark-act)         ;; pick some comfortable binding
-   ("C-;" . embark-dwim)        ;; good alternative: M-.
-   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-
-  :init
-
-  ;; Optionally replace the key help with a completing-read interface
-  (setq prefix-help-command #'embark-prefix-help-command)
-
-  :config
-
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
-
-;; Consult users will also want the embark-consult package.
-(use-package embark-consult
-  :ensure t
-  :after (embark consult)
-  :demand t ; only necessary if you have the hook below
-  ;; if you want to have consult previews as you move around an
-  ;; auto-updating embark collect buffer
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
-
+;; Grep
 (use-package wgrep
   :custom
   (wgrep-enable-key "e")
@@ -358,35 +209,6 @@
 	desktop-auto-save-timeout 600)
   (desktop-save-mode 1)
   (setq-default history-length 1000)
-  (setq desktop-globals-to-save
-	'((comint-input-ring        . 50)
-          (compile-history          . 30)
-          desktop-missing-file-warning
-          (dired-regexp-history     . 20)
-          (extended-command-history . 30)
-          (face-name-history        . 20)
-          (file-name-history        . 100)
-          (grep-find-history        . 30)
-          (grep-history             . 30)
-          (ido-buffer-history       . 100)
-          (ido-last-directory-list  . 100)
-          (ido-work-directory-list  . 100)
-          (ido-work-file-list       . 100)
-          (ivy-history              . 100)
-          (magit-read-rev-history   . 50)
-          (minibuffer-history       . 50)
-          (org-clock-history        . 50)
-          (org-refile-history       . 50)
-          (org-tags-history         . 50)
-          (query-replace-history    . 60)
-          (read-expression-history  . 60)
-          (regexp-history           . 60)
-          (regexp-search-ring       . 20)
-          register-alist
-          (search-ring              . 20)
-          (shell-command-history    . 50)
-          tags-file-name
-          tags-table-list))
   )
 
 (use-package session
@@ -400,30 +222,31 @@
 (savehist-mode)
 
 ;; Edit Utils
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq-default fill-column 100)
-(setq-default truncate-lines t)
-(electric-pair-mode)
-(electric-indent-mode)
-(global-so-long-mode t)
-(global-display-line-numbers-mode t)
-(column-number-mode t)
-(save-place-mode t)
-(which-function-mode t)
-(setq scroll-conservatively 100)
-(setq make-backup-files nil)
-(setq create-lockfiles nil)
-(setq auto-save-file-name-transforms
-      `((".*" "~/.emacs-saves/" t)))
-(setq uniquify-buffer-name-style 'reverse)
-(setq uniquify-separator " â€¢ ")
-(setq uniquify-after-kill-buffer-p t)
-(setq uniquify-ignore-buffers-re "^\\*")
-(global-prettify-symbols-mode)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq-default 
+ indent-tabs-mode nil
+ tab-width 4
+ fill-column 100
+ truncate-lines t
+ electric-pair-mode t
+ electric-indent-mode t
+ column-number-mode t
+ save-place-mode t
+ which-function-mode t
+ scroll-conservatively 100
+ make-backup-files nil
+ create-lockfiles nil
+ auto-save-file-name-transforms
+      `((".*" "~/.emacs-saves/" t))
+ uniquify-buffer-name-style 'reverse
+ uniquify-separator " â€¢ "
+ uniquify-after-kill-buffer-p t
+ uniquify-ignore-buffers-re "^\\*"
+)
 
-(use-package ibuffer-vc)
+(global-display-line-numbers-mode)
+(setq global-prettify-symbols-mode t)
+(setq global-so-long-mode t)
+
 (use-package vlf)
 (use-package beacon
   :delight
@@ -462,6 +285,7 @@
 
 (use-package goto-last-change)
 
+;; Languages
 (use-package plantuml-mode
   :mode "\\.uml\\'"
   :config
@@ -471,6 +295,8 @@
   (setq plantuml-default-exec-mode 'jar)
   (setq org-plantuml-jar-path plantuml-jar-path)
   )
+
+;; Org
 (use-package org
   :custom
   (org-confirm-babel-evalute nil)
@@ -512,7 +338,6 @@
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
 
-(use-package quelpa-use-package   :ensure t)
 (use-package org-download
   :quelpa (org-download :fetcher github :repo "baolonglin/org-download")
   )
@@ -716,22 +541,23 @@
    (c++-mode . eglot-ensure))
   )
 
-(use-package gptel
-  (setq-default gptel-model "gemma:7b"
-                gptel-backend (gptel-make-ollama "Ollama"
-                                :host "localhost:11434"
-                                :stream t
-                                :models '("gemma:7b")))
-  (gptel-make-openai "llama-cpp"
-    :stream t
-    :protocol "http"
-    :host "localhost:8080"
-    :models '("test"))
-  )
-
 (use-package zig-mode
   :mode ("\\.zig\\'" . zig-mode)
   )
+
+(defun config/chatgpt-el ()
+  (let* ((chatgpt-bin (expand-file-name "site-lisp/chatgpt-el/chatgpt" user-emacs-directory)))
+    (when (file-exists-p chatgpt-bin)
+      (autoload 'chatgpt-query "chatgpt" nil t)
+      (autoload 'chatgpt-insert-reply "chatgpt" nil t)
+      (global-set-key "\C-cq" 'chatgpt-query)
+      (global-set-key "\C-cQ" 'chatgpt-insert-reply)
+      (setq chatgpt-prog chatgpt-bin)
+      )
+    )
+  )
+
+(config/chatgpt-el)
 
 
 (use-package edit-server
@@ -749,5 +575,10 @@
 	        (define-key special-event-map [sigusr1] 'signal-restart-server)
             ))
 
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (provide 'init)
 (put 'upcase-region 'disabled nil)
+
